c3ca3b77aaa820990413bab88521797d5ae84f47
Krina.Patel <Krina.Patel@MUM02L10869>
2023-07-03 15:20:04 +0530

    Add project files.

---
+++ SalesOrderClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalesOrderDAL;
+using SalesOrderEntities;
+using SalesOrderExceptions;
+using SalesOrderManager;
+
+namespace SalesOrderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date, cname, spname;
+            double amount;
+            Console.WriteLine("Enter Order date (ddmmyyyy");
+            date= Console.ReadLine();
+            Console.WriteLine("Enter Order Amount:");
+            amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Customer Name:");
+            cname = Console.ReadLine();
+            Console.WriteLine("Enter Sales Person Name:");
+            spname = Console.ReadLine();
+
+            int choice;
+            Console.WriteLine("1. Add Orders\n2. Display Orders\n3.Exit");
+            Console.WriteLine("Enter the choice");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ SalesOrderDAL/OrderDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesOrderDAL
+{
+    public class OrderDetails
+    {
+        public string OrderDate { get; set; }
+        public string CustName { get; set; }
+        public string SalePersonName { get; set; }
+        public double OrderAmount { get; set; }
+
+        OrderDetails(string orderdate,double orderamount, string custname, string salepname)
+        {
+            this.OrderDate = orderdate;
+            this.OrderAmount = orderamount;
+            this.CustName = custname;
+            this.SalePersonName = salepname;
+        }
+
+        public void Display()
+        {
+
+        }
+    }
+}
